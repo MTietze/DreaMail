5c1faa4e57a7fd740786b656e3a0c551e4e050e7
Max Tietze <max@zipari.com>
2015-08-16 23:39:43 -0400

    added login page

--- dreamail_tracker/views.py
+++ dreamail_tracker/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+class DreamView(TemplateView):
+    template_name = "angular.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(DreamView, self).get_context_data(**kwargs)
+
+        return context
