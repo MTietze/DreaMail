f6035dc2b941fcc420485ee588426da22dd79727
Max Tietze <max@zipari.com>
2017-07-05 20:00:37 -0400

    updates npm packages

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule}   from '@angular/forms';
 import {HttpModule} from '@angular/http';
-import { InfiniteScrollModule } from 'angular2-infinite-scroll';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import {routing} from './app.routes';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
